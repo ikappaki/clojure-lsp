213b771fb71718fb5513449da7d3c8d494ee326f
ikappaki <ikappaki@users.noreply.github.com>
2022-09-30 20:45:54 +0100

    file names based on timestamp

--- clojure-lsp-profile.bat
+++ clojure-lsp-profile.bat
@@ -1,4 +1,4 @@
 @echo off
-SET MYOUT=".\123.txt"
-IF EXIST %MYOUT% DEL /F %MYOUT%
+rem SET MYOUT=".\123.txt"
+rem IF EXIST %MYOUT% DEL /F %MYOUT%
 powershell -NoProfile -File d:/src/clojure-lsp/scripts/process-usage.ps1 d:/src/clojure-lsp/clojure-lsp.exe %*
--- scripts/process-usage.ps1
+++ scripts/process-usage.ps1
@@ -1,25 +1,37 @@
 $exec, $exec_args = $args
 # echo $args.length "${exec_args}"
-$out = "123.txt"
+if (!$exec)
+{
+    Write-Host "No exec supplied, exiting ..."
+    Exit 55
+}
+
+
+$interval_ms = 200
+
+$logdir = "process-usage-logs"
+New-Item -Path $logdir -ItemType directory -Force | Out-Null
+$out = "${logdir}/$(get-date -Format ""FileDateTime"").txt"
+
 $proc = if ($exec_args) {Start-Process $exec $exec_args -NoNewWindow -passthru}
 	 else
 	 {Start-Process $exec -NoNewWindow -passthru}
-	
 
+filter timestamp {"$([Math]::Round((Get-Date).ToFileTime()/10000)) $_"}
+# filter timestamp {"$(Get-Date -Format o) $_"}
 
 try {
     if (get-process -id $proc.id -ErrorAction SilentlyContinue)
     {
-	get-process -id $proc.id| Format-Table | out-string |  % {$_.split([Environment]::NewLine).trim()[2]} | Add-content $out
+	"{:exec ""${exec}"" :interval-ms ${interval_ms} :args ""${exec_args}""}" | Add-content $out
+	get-process -id $proc.id -ErrorAction SilentlyContinue| Format-Table | out-string |  % {$_.split([Environment]::NewLine).trim()[2]} | Add-content $out
     }
     while (get-process -id $proc.id -ErrorAction SilentlyContinue)
     {
-	get-process -id $proc.id -ErrorAction SilentlyContinue | Format-Table -HideTableHeaders| out-string | % {$_.trim()} | Add-content $out 
-	# get-process -id $proc.id| select-object @{l='Private Memory (MB)'; e={$_.privatememorysize64 / 1mb}}| Format-Table -HideTableHeaders | out-string | Add-content "123.txt"
-	start-sleep 0.5
+	get-process -id $proc.id -ErrorAction SilentlyContinue | Format-Table -HideTableHeaders| out-string | % {$_.trim()} | timestamp | Add-content $out 
+	start-sleep -milliseconds $interval_ms
     }
 } finally {
     Stop-Process  $proc -ErrorAction SilentlyContinue
+    Write-host "`n:process-usage-output-path ${out}"
 }
-
-# # get-process -id $id | select-object @{l='Private Memory (MB)'; e={$_.privatememorysize64 / 1mb}}
